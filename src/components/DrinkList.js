8878845e8e606c5b0c1ee00b9dbd88d0da3bc126
cedric-winbush <cawinbushjr@gmail.com>
2020-02-19 16:48:15 -0600

    map drinks array and display

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+
+
 
 const DrinkList = props => {
   return (
-    <div>
-      <p>drinklist</p>
-    </div>
+    <>
+      {props.error ? (
+        <div>{props.error}</div>
+      ) : (
+        props.drinks.map(drink => (
+          <div key={drink.id}>
+            <img src= {drink.strDrinkThumb} alt= {drink.strDrink}/>
+            <h3>{drink.strDrink}</h3>
+          </div>
+        ))
+      )}
+
+
+
+    </>
   );
 };
-export default DrinkList;
+const mapStateToProps = state => {
+
+  return {
+    drinks: state.drinks,
+    error: state.error
+  }
+
+}
+
+export default connect(mapStateToProps, {})(DrinkList);
