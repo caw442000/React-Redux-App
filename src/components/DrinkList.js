4023645cbfe393d9b5123648f01254e3cb1847d0
cedric-winbush <cawinbushjr@gmail.com>
2020-02-19 16:56:01 -0600

    MVP styled

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,25 +1,60 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 
 
 
 const DrinkList = props => {
+
+  const DrinksListDiv = styled.div`
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30px;
+  align-items: center;
+  max-width: 100%;
+  flex-wrap: wrap;
+
+`;
+  const DrinkCardDiv = styled.div `
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    margin: 30px;
+    align-items: center;
+    max-width: 100%;
+    flex-direction: column;
+    width:200px; 
+    height: 300px;
+    border: 1px solid;
+    padding: 10px;
+  
+  
+  `
+
+  const DrinkImage = styled.img`
+    width: 150px;
+    height: 150px
+    object-fit: scale;
+    flex-shrink:3;    
+
+  `
   return (
-    <>
+    <DrinksListDiv>
       {props.error ? (
         <div>{props.error}</div>
       ) : (
         props.drinks.map(drink => (
-          <div key={drink.id}>
-            <img src= {drink.strDrinkThumb} alt= {drink.strDrink}/>
+          <DrinkCardDiv key={drink.id}>
+            <DrinkImage src= {drink.strDrinkThumb} alt= {drink.strDrink}/>
             <h3>{drink.strDrink}</h3>
-          </div>
+          </DrinkCardDiv>
         ))
       )}
 
 
 
-    </>
+    </DrinksListDiv>
   );
 };
 const mapStateToProps = state => {
