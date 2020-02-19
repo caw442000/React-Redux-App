c85d3a26499dc267457b6d347ffeaccd08d874b9
cedric-winbush <cawinbushjr@gmail.com>
2020-02-19 16:18:33 -0600

    connect store to reducer and middleware

--- src/components/DrinkForm.js
+++ src/components/DrinkForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const DrinkForm = props => {
+  
   return (
     <div>
       <p>DrinkForm</p>
---
+++ src/reducers/drinkReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  drinks: [],
+  isFetchingData: false,
+  error: ""
+};
+
+
+export const drinkReducer = (state = initialState, action) => {
+  switch(action.type) {
+
+    default:
+      return state;
+  }  
+
+
+}
